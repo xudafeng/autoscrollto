c8d6b10f080db0359bbbe6f165b8c8712cbbecfa
xudafeng <xudafeng@126.com>
2019-08-01 10:58:01 +0800

    chore: add testcase

--- test/autoscrollto.test.js
+++ test/autoscrollto.test.js
@@ -1,9 +1,44 @@
 'use strict';
 
 const assert = require('assert');
+const { webpackHelper } = require('macaca-wd');
 
-const AutoScrollTo = require('../lib/autoscrollto');
+const {
+  driver
+} = webpackHelper;
+
+const BASE_URL = 'http://127.0.0.1:8000/test';
 
 describe('./test/autoscrollto.test.js', () => {
-  let res;
+  before(() => {
+    return driver
+      .initWindow({
+        width: 375,
+        height: 600,
+        deviceScaleFactor: 2
+      });
+  });
+
+  beforeEach(() => {
+  });
+
+  afterEach(function () {
+    return driver
+      .coverage()
+      .saveScreenshots(this);
+  });
+
+  after(() => {
+    return driver
+      .openReporter(false)
+      .quit();
+  });
+
+  describe('autoscrollto', () => {
+    it('render should be ok', () => {
+      return driver
+        .getUrl(`${BASE_URL}`)
+        .sleep(5000);
+    });
+  });
 });
